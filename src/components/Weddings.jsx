4658735b5df5cb6cd1040ad93f486ea8c64b73d0
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-10-09 19:37:29 +0600

    Footer section created

---
+++ src/components/Weddings.jsx
@@ -0,0 +1,9 @@
+
+const Weddings = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default Weddings;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../components/Banner";
-import Features from "../components/Features";
+import Footer from "../components/Footer";
+import Services from "../components/Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <Features></Features>
+            <Services></Services>
+            <Footer></Footer>
         </div>
     );
 };
